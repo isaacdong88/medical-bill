d2a921fde7a7dcb0a79ae7abf53a44719120086c
isaacdong88 <isaacdong88@gmail.com>
2023-06-20 23:20:01 -0400

    add more field into form, work on media query

--- frontend/src/pages/MedicalBill.jsx
+++ frontend/src/pages/MedicalBill.jsx
@@ -7,6 +7,9 @@ function MedicalBill() {
     firstname: "",
     lastname: "",
     address: "",
+    state: "",
+    city: "",
+    zipcode: "",
     hospital: "",
     servicedate: "",
     billamount: "",
@@ -47,6 +50,14 @@ function MedicalBill() {
       </div>
       <div>
         <form className="bill-form" onSubmit={handleSubmit}>
+          <div className="form-header">
+            <h2>Truffle Health Bill Reduction</h2>
+            <br />
+            <p>
+              Medical bills can be confusing and overwhelming to deal with.
+              We're here to help.
+            </p>
+          </div>
           <div className="input-field">
             <label>
               First Name <span className="asterisk">*</span>
@@ -90,6 +101,112 @@ function MedicalBill() {
               required
               onChange={handleChange}
             />
+          </div>
+
+          <div className="address-field">
+            <div className="address">
+              <label>
+                State <span className="asterisk">*</span>
+              </label>
+              <br />
+              <select name="state" id="state" onChange={handleChange}>
+                <option value="" selected disabled hidden>
+                  Choose State
+                </option>
+                <option value="Alabama">Alabama</option>
+                <option value="Alaska">Alaska</option>
+                <option value="Arizona">Arizona</option>
+                <option value="Arkansas">Arkansas</option>
+                <option value="California">California</option>
+                <option value="Colorado">Colorado</option>
+                <option value="Connecticut">Connecticut</option>
+                <option value="Delaware">Delaware</option>
+                <option value="District Of Columbia">
+                  District Of Columbia
+                </option>
+                <option value="Florida">Florida</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Hawaii">Hawaii</option>
+                <option value="Idaho">Idaho</option>
+                <option value="Illinois">Illinois</option>
+                <option value="Indiana">Indiana</option>
+                <option value="Iowa">Iowa</option>
+                <option value="Kansas">Kansas</option>
+                <option value="Kentucky">Kentucky</option>
+                <option value="LLouisiana">Louisiana</option>
+                <option value="Maine">Maine</option>
+                <option value="Maryland">Maryland</option>
+                <option value="Massachusetts">Massachusetts</option>
+                <option value="Michigan">Michigan</option>
+                <option value="Minnesota">Minnesota</option>
+                <option value="Mississippi">Mississippi</option>
+                <option value="Missouri">Missouri</option>
+                <option value="Montana">Montana</option>
+                <option value="Nebraska">Nebraska</option>
+                <option value="Nevada">Nevada</option>
+                <option value="New Hampshire">New Hampshire</option>
+                <option value="New Jersey">New Jersey</option>
+                <option value="New Mexico">New Mexico</option>
+                <option value="New York">New York</option>
+                <option value="North Carolina">North Carolina</option>
+                <option value="North Dakota">North Dakota</option>
+                <option value="Ohio">Ohio</option>
+                <option value="Oklahoma">Oklahoma</option>
+                <option value="Oregon">Oregon</option>
+                <option value="Pennsylvania">Pennsylvania</option>
+                <option value="Rhode Island">Rhode Island</option>
+                <option value="South Carolina">South Carolina</option>
+                <option value="South Dakota">South Dakota</option>
+                <option value="Tennessee">Tennessee</option>
+                <option value="Texas">Texas</option>
+                <option value="Utah">Utah</option>
+                <option value="Vermont">Vermont</option>
+                <option value="Virginia">Virginia</option>
+                <option value="Washington">Washington</option>
+                <option value="West Virginia">West Virginia</option>
+                <option value="Wisconsin">Wisconsin</option>
+                <option value="Wyoming">Wyoming</option>
+              </select>
+
+              {/* <input
+                type="text"
+                id="state"
+                name="state"
+                value={medicalForm.state}
+                required
+                onChange={handleChange}
+              /> */}
+            </div>
+
+            <div className="address">
+              <label>
+                City <span className="asterisk">*</span>
+              </label>
+              <br />
+              <input
+                type="text"
+                id="city"
+                name="city"
+                value={medicalForm.city}
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="address">
+              <label>
+                Zip Code <span className="asterisk">*</span>
+              </label>
+              <br />
+              <input
+                type="text"
+                id="zipcode"
+                name="zipcode"
+                value={medicalForm.zipcode}
+                required
+                onChange={handleChange}
+              />
+            </div>
           </div>
 
           <div className="input-field">
